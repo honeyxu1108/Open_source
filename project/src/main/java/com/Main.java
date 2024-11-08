18529ff5543e77be3ba30c8498377253cc839cf2
honeyxu1108 <honeyxu1108@163.com>
2024-11-08 14:43:57 +0800

    Introduce a syntax error

--- project/src/main/java/com/Main.java
+++ project/src/main/java/com/Main.java
@@ -25,7 +25,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		Main calculator = new Main();
+		Main calculator = new Main()
 		
 		// Test cases
 		System.out.println("Test Case 1 (5 + 5): " + calculator.add(5, 5));
